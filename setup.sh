4c27fe969c08522c627dcfb15cbf605eea2ef169
Shane Kennedy <shane.kennedy19@gmail.com>
2024-09-04 15:07:53 +0200

    Add brewfile

--- setup.sh
+++ setup.sh
@@ -2,9 +2,13 @@
 
 DOTTIES=$(pwd)
 
+# Brew
+ln -s "$DOTTIES/Brewfile" "$HOME/Brewfile"
+
 # Git
 ln -s "$DOTTIES/gitconfig" "$HOME/.gitconfig"
 
 # Zsh
 ln -s "$DOTTIES/zshrc" "$HOME/.zshrc"
 
+echo "Now run brew bundle install and restart your shell"
