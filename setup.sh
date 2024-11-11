a919018a25c3d5c6c4f9feaa83ea40885b33a80b
Shane Kennedy <shane.kennedy19@gmail.com>
2024-11-11 21:06:55 +0100

    Add nvim bootstrap

--- setup.sh
+++ setup.sh
@@ -11,4 +11,6 @@ ln -s "$DOTTIES/gitconfig" "$HOME/.gitconfig"
 # Zsh
 ln -s "$DOTTIES/zshrc" "$HOME/.zshrc"
 
+git clone git@github.com:shaneikennedy/nvim.git ~/.config/nvim
+
 echo "Now run brew bundle install and restart your shell"
